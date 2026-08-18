681010a7f2088256ab0beb84e30f723ee2b1b3ef
abner-devv <abner.santana.dev@gmail.com>
2026-08-17 21:30:34 -0300

    Exercicio Tipos Primitivos

---
+++ maratona-java/src/com/devdojo/maratonajava/introducao/Aula01OlaDevDojo.java
@@ -0,0 +1,7 @@
+package com.devdojo.maratonajava.introducao;
+
+public class Aula01OlaDevDojo {
+    public static void main() {
+        System.out.println("Olá Dev Dojo!");
+    }
+}
---
+++ maratona-java/src/com/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -0,0 +1,15 @@
+package com.devdojo.maratonajava.introducao;
+
+public class Aula02TiposPrimitivos {
+    static void main() {
+        //int, double, float, chat, string, short, long, boolean
+        int n = 1;
+        long l = 1000;
+        short s = 128;
+        double d = 1.3d;
+        float f = 13.5f;
+        boolean b = true;
+        String str = "string";
+        char c = 'A';
+    }
+}
